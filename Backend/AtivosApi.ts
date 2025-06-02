123048b5c1a57a1ed160f3fb5f3956d254cba9f8
Ryan <ryandsferrer@gmail.com>
2025-06-01 22:53:38 -0300

    Api with itens running and return reponse

---
+++ Backend/AtivosApi.ts
@@ -0,0 +1,18 @@
+import Fastify from 'fastify'
+import fs from 'fs/promises'
+
+const ServeAtivos= Fastify({logger: true})//Servdidor e status de execulcao
+
+ServeAtivos.get("/", async (req, res)=>{
+    const dataItens= (await fs.readFile('./Ativos/Ativos.json')).toString()//acessando os itens e transformando em tipo string
+
+    const Resdata= JSON.parse(dataItens)
+
+    res.send(Resdata)
+})
+
+const port=  Number(process.env.PORT) || 3000
+
+ServeAtivos.listen({ port }, ()=>{
+    console.log('rodando na porta: ' + port)
+})
